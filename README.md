89d7edc146497f7e26a7b1a98c50e06797d82534
kyle-koch <kochkd15@gmail.com>
2023-05-23 15:03:14 -0700

    uh, I forgot to commit... so here's the rest of the code!

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # js-fundamentals-3-todo
+This is uh... not the prettiest code I've ever written. Past a certain point, getting it to work at all took priority over brevity.
+
+It should be fairly intuitive to run, just toss your task in the To-Do box, select your priority, add in some details if you want, and click submit.
+
+Clear tasks gets rid of them, and clear storage gets rid of them from local memory.
--- framework.js
+++ framework.js
@@ -1,30 +1,146 @@
-console.log('My code is running');
+var clearTasksButton = document.getElementById("clearTasksButton");
+var clearStorageButton = document.getElementById("clearStorageButton");
 
-//let taskEntry = prompt('Your task:', 'task here...');
+clearTasksButton.addEventListener("click", function() {
+  var highPriorityList = document.getElementById("highPriorityList");
+  var normalPriorityList = document.getElementById("normalPriorityList");
+  highPriorityList.innerHTML = "";
+  normalPriorityList.innerHTML = "";
 
-document.getElementById("todoForm").addEventListener("submit", function(event) {event.preventDefault(); // Prevent form submission
+  clearStoredTodos();
+});
 
-var task = document.getElementById("task").value;
-var priority = document.getElementById("priority").value;
-var details = document.getElementById("details").value;
+clearStorageButton.addEventListener("click", function() {
+  localStorage.clear();
+});
 
-var todo = {
-  task: task,
-  priority: priority,
-  details: details
-};
+document.getElementById("todoForm").addEventListener("submit", function(event) {
+  event.preventDefault();
 
-if (todo.priority === "high") {
+  var task = document.getElementById("task").value;
+  var priority = document.getElementById("priority").value;
+  var details = document.getElementById("details").value;
+
+  var todo = {
+    task: task,
+    priority: priority,
+    details: details,
+    done: false
+  };
+
+  if (todo.priority === "high") {
     var highPriorityList = document.getElementById("highPriorityList");
     var listItem = document.createElement("li");
-    listItem.textContent = "Task: " + todo.task + " | Priority: " + todo.priority + " | Details: " + todo.details;
+
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function() {
+      if (checkbox.checked) {
+        taskText.style.textDecoration = "line-through";
+        todo.done = true;
+      } else {
+        taskText.style.textDecoration = "none";
+        todo.done = false;
+      }
+    });
+
+    var taskText = document.createElement("span");
+    taskText.innerHTML = "Task: " + todo.task + "<br/>";
+    var detailsText = document.createElement("span");
+    detailsText.innerHTML = "Details: " + todo.details;
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(taskText);
+    listItem.appendChild(detailsText);
     highPriorityList.appendChild(listItem);
   } else {
     var normalPriorityList = document.getElementById("normalPriorityList");
     var listItem = document.createElement("li");
-    listItem.textContent = "Task: " + todo.task + " | Priority: " + todo.priority + " | Details: " + todo.details;
+
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function() {
+      if (checkbox.checked) {
+        taskText.style.textDecoration = "line-through";
+        todo.done = true;
+      } else {
+        taskText.style.textDecoration = "none";
+        todo.done = false;
+      }
+    });
+
+    var taskText = document.createElement("span");
+    taskText.innerHTML = "Task: " + todo.task + "<br/>";
+    var detailsText = document.createElement("span");
+    detailsText.innerHTML = "Details: " + todo.details;
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(taskText);
+    listItem.appendChild(detailsText);
     normalPriorityList.appendChild(listItem);
   }
 
-document.getElementById("todoForm").reset();
+  var todos = JSON.parse(localStorage.getItem("todos")) || [];
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  document.getElementById("todoForm").reset();
+});
+
+window.addEventListener("DOMContentLoaded", function() {
+  var todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  todos.forEach(function(todo) {
+    if (todo.priority === "high") {
+      var highPriorityList = document.getElementById("highPriorityList");
+      var listItem = document.createElement("li");
+
+      var checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.addEventListener("change", function() {
+        if (checkbox.checked) {
+          taskText.style.textDecoration = "line-through";
+        } else {
+          taskText.style.textDecoration = "none";
+        }
+      });
+
+      var taskText = document.createElement("span");
+      taskText.innerHTML = "Task: " + todo.task + "<br/>";
+      var detailsText = document.createElement("span");
+      detailsText.innerHTML = "Details: " + todo.details;
+
+      checkbox.checked = todo.done;
+
+      listItem.appendChild(checkbox);
+      listItem.appendChild(taskText);
+      listItem.appendChild(detailsText);
+      highPriorityList.appendChild(listItem);
+    } else {
+      var normalPriorityList = document.getElementById("normalPriorityList");
+      var listItem = document.createElement("li");
+
+      var checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.addEventListener("change", function() {
+        if (checkbox.checked) {
+          taskText.style.textDecoration = "line-through";
+        } else {
+          taskText.style.textDecoration = "none";
+        }
+      });
+
+      var taskText = document.createElement("span");
+      taskText.innerHTML = "Task: " + todo.task + "<br/>";
+      var detailsText = document.createElement("span");
+      detailsText.innerHTML = "Details: " + todo.details;
+
+      checkbox.checked = todo.done;
+
+      listItem.appendChild(checkbox);
+      listItem.appendChild(taskText);
+      listItem.appendChild(detailsText);
+      normalPriorityList.appendChild(listItem);
+    }
+  });
 });
